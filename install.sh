87ed19732d947be8e09225fec4adae4adf4c32dd
Yuki Matsushita <katsumeshi@gmail.com>
2022-07-16 17:42:26 -0700

    add mac os setup

--- install.sh
+++ install.sh
@@ -33,14 +33,13 @@ brew bundle --file ~/dotfiles/Brewfile
 
 LIBRARY_DIR="$HOME/Library"
 PREFERENCE_DIR="$LIBRARY_DIR/Preferences"
-
-echo "Setup VSCode..."
 VSCODE_PREFS_DIR="$LIBRARY_DIR/Application Support/Code/User"
 mkdir -p $VSCODE_PREFS_DIR
 
-echo "Symlinking dotfiles..."
 ln -sf ~/dotfiles/config ~/.config
 ln -sf ~/dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/zshrc ~/.zshrc
 ln -sf ~/dotfiles/vscode/settings.json "$VSCODE_PREFS_DIR/settings.json"
 ln -sf ~/dotfiles/iterm2/com.googlecode.iterm2.plist "$PREFERENCE_DIR/com.googlecode.iterm2.plist"
+
+source ~/dotfiles/macos
