0afeba8c756a1aa2a393d428b14073d0f494ef71
Yuki Matsushita <katsumeshi@gmail.com>
2022-07-17 01:16:02 -0700

    update

--- install.sh
+++ install.sh
@@ -1,25 +1,27 @@
 # #!/bin/sh
 
-# # Install Rosseta2
-# if [[ "$(uname -m)" == arm64 ]]; then
-#   softwareupdate --install-rosetta --agree-to-license
-# fi
-
-# # Setup Homebrew
-# if [ -x "$(command -v brew)" ]; then
-#   brew update
-# else
-#   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-#   if [[ "$(uname -m)" == arm64 ]]; then # M1
-#     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-#     eval "$(/opt/homebrew/bin/brew shellenv)"
-#     echo "⚠️ You might reboot the machine... if brew is not found"
-#   elif [[ "$(uname -m)" == x86_64 ]]; then # Intel
-#     eval "$(/usr/local/bin/brew shellenv)"
-#   fi
-# fi
 
 DOTFILES_PATH=$HOME/dotfiles
+
+# Install Rosseta2
+if [[ "$(uname -m)" == arm64 ]]; then
+  softwareupdate --install-rosetta --agree-to-license
+fi
+
+# Setup Homebrew
+if [ -x "$(command -v brew)" ]; then
+  brew update
+else
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  if [[ "$(uname -m)" == arm64 ]]; then # M1
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    echo "⚠️ You might reboot the machine... if brew is not found"
+  elif [[ "$(uname -m)" == x86_64 ]]; then # Intel
+    eval "$(/usr/local/bin/brew shellenv)"
+  fi
+fi
+
 
 # Setup .dotfiles
 if [ -d $DOTFILES_PATH ]; then
@@ -28,11 +30,9 @@ else
   git clone git@github.com:katsumeshi/dotfiles.git $DOTFILES_PATH
 fi
 
+# Brew
+brew bundle --file $DOTFILES_PATH/Brewfile
 
-
-# # Brew
-# brew bundle --file $DOTFILES_PATH/Brewfile
-
-sh $DOTFILES_PATH/.symlinks
+zsh $DOTFILES_PATH/.symlinks
 
 source $HOME/.zshrc
