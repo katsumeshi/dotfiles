6938ff9338313565e693347a230d8db117ec990f
Yuki Matsushita <katsumeshi@gmail.com>
2022-12-25 20:25:53 -0800

    feat: add options

--- .config/nvim/lua/katsumeshi/init.lua
+++ .config/nvim/lua/katsumeshi/init.lua
@@ -1,1 +1,2 @@
 require("katsumeshi.remap")
+require("katsumeshi.options")
---
+++ .config/nvim/lua/katsumeshi/options.lua
@@ -0,0 +1,14 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+vim.opt.backup = false
+
