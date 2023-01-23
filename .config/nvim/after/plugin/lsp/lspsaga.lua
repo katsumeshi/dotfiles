5f004bef4778d4248c34ebbb9be706ab22867e6f
Yuki Matsushita <katsumeshi@gmail.com>
2023-01-22 16:20:29 -0800

    update dotfiles

--- .config/nvim/after/plugin/lsp/lspsaga.lua
+++ .config/nvim/after/plugin/lsp/lspsaga.lua
@@ -8,11 +8,14 @@ saga.setup({
 	-- keybinds for navigation in lspsaga window
 	move_in_saga = { prev = "<C-k>", next = "<C-j>" },
 	-- use enter to open file with finder
-	finder_action_keys = {
+	finder = {
 		open = "<CR>",
 	},
 	-- use enter to open file with definition preview
-	definition_action_keys = {
+	definition = {
 		edit = "<CR>",
+		vsplit = "sv",
+		split = "sh",
+		tabe = "t",
 	},
 })
--- .config/nvim/lua/katsumeshi/options.lua
+++ .config/nvim/lua/katsumeshi/options.lua
@@ -29,3 +29,5 @@ opt.clipboard:append("unnamedplus")
 
 opt.splitright = true
 opt.splitbelow = true
+
+opt.swapfile = false
