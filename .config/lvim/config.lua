94d401c9e9e49b9497fd16728f55c32331db8f5a
Yuki Matsushita <katsumeshi@gmail.com>
2023-05-06 16:19:11 -0700

    update lvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -12,9 +12,9 @@ vim.opt.shell = "/bin/sh"
 -- general
 lvim.log.level = "info"
 lvim.format_on_save = {
-  enabled = true,
-  pattern = "*.lua",
-  timeout = 1000,
+	enabled = true,
+	-- pattern = "*.lua",
+	timeout = 1000,
 }
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
@@ -62,7 +62,7 @@ lvim.builtin.treesitter.auto_install = true
 -- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. IMPORTANT: Requires `:LvimCacheReset` to take effect
 -- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
 -- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
---   return server ~= "emmet_ls"
+--   return server ~= "emmet_ls       "
 -- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
@@ -76,18 +76,18 @@ lvim.builtin.treesitter.auto_install = true
 -- end
 
 -- -- linters, formatters and code actions <https://www.lunarvim.org/docs/configuration/language-features/linting-and-formatting>
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "stylua" },
-  {
-    command = "prettier",
-    filetypes = { "typescript", "typescriptreact" },
-  },
-}
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "eslint", filetypes = { "typescript", "typescriptreact" } }
-}
+local formatters = require("lvim.lsp.null-ls.formatters")
+formatters.setup({
+	{ command = "stylua" },
+	{
+		command = "prettier",
+		filetypes = { "typescript", "typescriptreact" },
+	},
+})
+local linters = require("lvim.lsp.null-ls.linters")
+linters.setup({
+	{ command = "eslint", filetypes = { "typescript", "typescriptreact" } },
+})
 --   { command = "flake8", filetypes = { "python" } },
 --   {
 --     command = "shellcheck",
@@ -118,3 +118,62 @@ linters.setup {
 --     require("nvim-treesitter.highlight").attach(0, "bash")
 --   end,
 -- })
+--
+lvim.builtin.dap.active = true
+local dap = require("dap")
+
+dap.adapters.go = function(callback, _)
+	local stdout = vim.loop.new_pipe(false)
+	local handle
+	local pid_or_err
+	local port = 38697
+	local opts = {
+		stdio = { nil, stdout },
+		args = { "dap", "-l", "127.0.0.1:" .. port },
+		detached = true,
+	}
+	handle, pid_or_err = vim.loop.spawn("dlv", opts, function(code)
+		stdout:close()
+		handle:close()
+		if code ~= 0 then
+			print("dlv exited with code", code)
+		end
+	end)
+	assert(handle, "Error running dlv: " .. tostring(pid_or_err))
+	stdout:read_start(function(err, chunk)
+		assert(not err, err)
+		if chunk then
+			vim.schedule(function()
+				require("dap.repl").append(chunk)
+			end)
+		end
+	end)
+	-- Wait for delve to start
+	vim.defer_fn(function()
+		callback({ type = "server", host = "127.0.0.1", port = port })
+	end, 100)
+end
+-- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+dap.configurations.go = {
+	{
+		type = "go",
+		name = "Debug",
+		request = "launch",
+		program = "${file}",
+	},
+	{
+		type = "go",
+		name = "Debug test", -- configuration for debugging test files
+		request = "launch",
+		mode = "test",
+		program = "${file}",
+	},
+	-- works with go.mod packages and sub packages
+	{
+		type = "go",
+		name = "Debug test (go.mod)",
+		request = "launch",
+		mode = "test",
+		program = "./${relativeFileDirname}",
+	},
+}
