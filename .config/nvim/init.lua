49a9889081a83335d927cdf45a8f9de522c53371
Yuki Matsushita <katsumeshi@gmail.com>
2024-05-03 22:45:14 -0700

    feat: update neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,6 @@
-vim.cmd[[source $HOME/.config/nvim/vscode/settings.vim]]
+if vim.g.vscode then
+	vim.cmd[[source $HOME/.config/nvim/vscode/settings.vim]]
+else
+	require("katsumeshi.core")
+	require("katsumeshi.lazy")
+end
---
+++ .config/nvim/lua/katsumeshi/core/options.lua
@@ -0,0 +1,11 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true -- To insert space characters whenever the tab key is pressed
+opt.autoindent = true
