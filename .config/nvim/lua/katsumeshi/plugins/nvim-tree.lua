91497f42e2bb1f006061e41c7dc97186106b28b2
Yuki Matsushita <katsumeshi@gmail.com>
2024-05-22 20:15:48 -0700

    feat: add trouble

--- .config/nvim/lua/katsumeshi/plugins/nvim-tree.lua
+++ .config/nvim/lua/katsumeshi/plugins/nvim-tree.lua
@@ -20,7 +20,7 @@ return {
 
     local k = vim.keymap
 
-    k.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+    k.set("n", "<leader>ee", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     k.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     k.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 
