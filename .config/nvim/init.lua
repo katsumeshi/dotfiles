b5801e1be5bc352c14c9bfa64b413e8ef9f04de4
Yuki Matsushita <katsumeshi@gmail.com>
2024-05-23 20:56:10 -0700

    feat: add lint/format

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,5 @@
 if vim.g.vscode then
-	vim.cmd[[source $HOME/.config/nvim/vscode/settings.vim]]
+	vim.cmd([[source $HOME/.config/nvim/vscode/settings.vim]])
 else
 	require("katsumeshi.core")
 	require("katsumeshi.lazy")
