91667201e7f5c4f3e92544d858332bed20d507c4
Yuki Matsushita <katsumeshi@gmail.com>
2024-06-01 22:24:00 -0700

    chore: add macro status into lualine

--- .config/nvim/lua/katsumeshi/plugins/lualine.lua
+++ .config/nvim/lua/katsumeshi/plugins/lualine.lua
@@ -1,14 +1,23 @@
 return {
-  'nvim-lualine/lualine.nvim',
-   dependencies = { 'nvim-tree/nvim-web-devicons' },
-   config = function ()
-     local l = require('lualine')
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local l = require("lualine")
 
-     l.setup({
-          options = {
-              theme = "catppuccin"
-              -- ... the rest of your lualine config
-          }
-      })
-   end
+		l.setup({
+			options = {
+				theme = "catppuccin",
+				-- ... the rest of your lualine config
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.statusline.mode.get,
+						cond = require("noice").api.statusline.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
 }
