7cad183ad46a3e19f4b8584651a56989bf4827d5
Yuki Matsushita <katsumeshi@gmail.com>
2024-06-08 21:44:00 -0700

    feat: add nvim surround

--- .config/nvim/lua/katsumeshi/plugins/color_tokyonight.lua
+++ .config/nvim/lua/katsumeshi/plugins/color_tokyonight.lua
@@ -7,6 +7,10 @@ return {
 		require("tokyonight").setup({
 			style = "storm",
 			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
 		})
 
 		vim.cmd("colorscheme tokyonight")
