db5058d7d1e26f3409d975b1f1e42145ff64781b
Yuki Matsushita <katsumeshi@gmail.com>
2024-06-10 22:37:14 -0700

    fix: trouble

--- .config/nvim/lua/katsumeshi/plugins/noice.lua
+++ .config/nvim/lua/katsumeshi/plugins/noice.lua
@@ -6,4 +6,16 @@ return {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
 	},
+	config = function()
+		local noice
+		noice = noice.setup({
+			views = {
+				notify = { replace = true },
+			},
+		})
+
+		vim.keymap.set("n", "<leader>nd", function()
+			noice.cmd("dismiss")
+		end)
+	end,
 }
--- .config/nvim/lua/katsumeshi/plugins/trouble.lua
+++ .config/nvim/lua/katsumeshi/plugins/trouble.lua
@@ -1,12 +1,18 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-  keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
-  },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{ "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>", desc = "Diagnostics (Trouble)" },
+		{ "<leader>xb", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Buffer Diagnostics (Trouble)" },
+		{
+			"<leader>xl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{ "<leader>xq", "<cmd>Trouble qflist toggle<CR>", desc = "Quickfix List (Trouble)" },
+		{ "<leader>xL", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+		{ "<leader>xt", "<cmd>TodoTrouble<CR>", desc = "Open todos in trouble" },
+	},
 }
