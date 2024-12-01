1d19f07f218807b21518729f6fd99700b488dfab
Yuki Matsushita <katsumeshi@gmail.com>
2024-11-30 21:41:13 -0800

    chore: clean up configs

--- .config/nvim/lua/katsumeshi/plugins/noice.lua
+++ .config/nvim/lua/katsumeshi/plugins/noice.lua
@@ -9,8 +9,27 @@ return {
 	config = function()
 		local noice = require("noice")
 		noice.setup({
-			views = {
-				notify = { replace = true },
+			-- original setup
+			-- views = {
+			-- 	notify = { replace = true },
+			-- },
+			--
+			-- suggested setup
+			lsp = {
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false, -- add a border to hover docs and signature help
 			},
 		})
 
