7292daba072947f4174af5691b2ebb2cd87783a7
Yuki Matsushita <katsumeshi@gmail.com>
2024-11-30 20:43:52 -0800

    chore: update nvim and plugins updates

--- .config/nvim/lua/katsumeshi/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/katsumeshi/plugins/lsp/lspconfig.lua
@@ -63,7 +63,7 @@ return {
 				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				keymap.set("n", "<leader>lr", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 			end,
 		})
 
